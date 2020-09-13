6c5aa0ccb421df5f4bee4518201a17ae511510c2
Kaneko Kenya <kaneko@atsu-maru.co.jp>
2020-09-13 20:15:12 +0900

    first commit

---
+++ Book.java
@@ -0,0 +1,16 @@
+class Book {
+    private String title;
+    // private final String author;
+    // private int price;
+
+    // void addPrice(int delta) {
+    //     price += delta;
+    // }
+}
+
+// class Test {
+//     public static void main (String[] args) {
+//         System.out.println("Hello");
+//     }
+// }
+
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "012" + "345";
+        System.out.println(s);
+    }
+}
